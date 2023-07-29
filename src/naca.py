4df87e73008f56b7ac00bde872a7d6b73a3f181b
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2023-07-29 12:16:17 -0500

    initial commit

---
+++ src/naca.py
@@ -0,0 +1,151 @@
+from typing import Union
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Naca:
+    # four digits naca
+    def __init__(self,digits: Union[list,str], chord: Union[float,int],points: int) -> None:
+        self.digits = digits
+        self.symmetrical = False
+        self.chord = chord
+        self.points = points
+        self.set_type()
+
+        self.x_coordinates = ()
+        self.y_coordinates = ()
+    
+
+    def export(self):
+        filename = "src/output/"+ "NACA " + str(self.digits) +".csv"
+        print("Saving txt file with all the coordinates")
+        with open(filename,"w") as outputfile:
+            for x,y in zip(self.x_coordinates[0][::-1],self.y_coordinates[0][::-1]):
+                print(f"{x},{y}",file = outputfile)     
+            for x,y in zip(self.x_coordinates[1][1::],self.y_coordinates[1][1::]):
+                print(f"{x},{y}",file = outputfile) 
+    # source: https://archive.aoe.vt.edu/mason/Mason_f/CAtxtAppA.pdf
+    def plot(self):
+        if self.family == 4:
+            # generating x values
+            x = np.linspace(0,self.chord,self.points)
+
+            yt = np.array(list(map(self.thickness_distribution,x)))
+
+            yc = np.array(list(map(self.camber_line,x)))
+
+            dyc = np.array(list(map(self.camber_slope,x)))
+
+            theta = np.arctan(dyc)
+
+            plt.plot(x,yt)
+            plt.plot(x,yc)
+
+            xu = x - yt * np.sin(theta)
+            xl = x + yt * np.sin(theta)
+
+            yl = yc - yt * np.cos(theta)
+            yu = yc + yt * np.cos(theta)
+
+
+            self.x_coordinates = (xl,xu)
+
+            self.y_coordinates = (yl,yu)
+
+
+
+            plt.plot(xu,yu)
+            plt.plot(xl,yl)
+
+            plt.xlim(0,self.chord )
+            plt.ylim(-self.thickness * 1.5,self.thickness * 1.5)
+
+           
+            
+            plt.show()
+    
+    def thickness_distribution(self,x):
+        term1 =  0.2969 * (np.sqrt(x/self.chord))
+        term2 = -0.1260 * (x / self.chord) 
+        term3 = -0.3516 * np.power(x/self.chord,2)
+        term4 =  0.2843 * np.power(x/self.chord,3)
+        term5 = -0.1015 * np.power(x/self.chord,4)
+        return self.thickness / 0.2 *  (term1 + term2 + term3 + term4 + term5)
+
+    
+       
+    def camber_line(self, x ):
+        if x >= 0 and x <=  self.camber_location * self.chord:
+            return self.maximum_camber / (self.camber_location ** 2) * ((2 * self.camber_location * (x/self.chord) - (x/self.chord)**2))
+        else:
+            return  (self.maximum_camber * ((1 - 2 * self.camber_location) + 2 * self.camber_location * (x/self.chord) - (x/self.chord) ** 2)) / (1 - self.camber_location)**2
+
+    def camber_slope(self,x):
+        if x >=0 and x <=  self.camber_location * self.chord:
+            return (2.0 * self.maximum_camber) / np.power(self.camber_location,2) * (self.camber_location - (x / self.chord))
+        else:
+            return (2.0 * self.maximum_camber) / np.power(1-self.camber_location,2) * (self.camber_location - (x / self.chord))
+
+
+
+
+    # thickness distribution
+    # mean camber line
+    # camber slope
+
+        # for 5 digits
+    # identifies the corresponding naca family
+    def set_type(self):
+        
+        # four digit series
+        if len(self.digits) == 4:
+            self.family = 4
+            self.maximum_camber = (int(self.digits[0]) / 100 ) 
+            self.camber_location = (int(self.digits[1])/10) 
+            self.thickness = (int(self.digits[2:])/ 100) 
+            
+
+
+        # five digit series
+        elif len(self.digits) == 5:
+            self.family = 5
+            self.design_cl = (3 / 2) * int(self.digits[0]) / 10
+            self.maximum_camber =  int(self.digits[1:3]) / 200
+            self.thickness =  int(self.digits[-2:]) / 100
+            
+
+    
+
+    def __str__(self) -> str:
+
+        if self.family == 5:
+            return f"""
+                {self.family} digits NACA
+                chord: {self.chord}
+                maximum camber: {self.maximum_camber}
+                thickness: {self.thickness}
+                design lift coefficient: {self.design_cl}
+                points: {self.points}
+
+                """
+        elif self.family == 4:
+            return f"""
+                {self.family} digits NACA
+                chord: {self.chord}
+                maximum camber: {self.maximum_camber}
+                thickness: {self.thickness}
+                camber location: {self.thickness}
+                points: {self.points}
+
+
+
+                """
+            
+
+
+jean = Naca("2432",5,200)
+
+print(jean)
+jean.plot()
+jean.export()
+
+
